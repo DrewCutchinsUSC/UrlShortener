75f9ebf03c805bd047792a7de5de03934fa1c871
Drew Cutchins <drew.cutch@gmail.com>
2020-09-16 20:29:32 -0700

    Fix field access in js

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -15,7 +15,7 @@ function submitUrl(){
     xmlhttp.open("POST", "api/url", true);
     xmlhttp.onreadystatechange = function () { //Call a function when the state changes.
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            displayResult(JSON.parse(xmlhttp.responseText).endpoint);               
+            displayResult(JSON.parse(xmlhttp.responseText).code);               
         }
     };
 
